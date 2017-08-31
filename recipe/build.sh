332929a6269af6e7bfc97299de2ce2770fc5ae89
Ray Donnelly <mingw.android@gmail.com>
2017-08-31 01:09:56 -0700

    Back to stable 1.10.22 version, darwin fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-CPPFLAGS=-I${PREFIX}/include \
-LDFLAGS=-L${PREFIX}/lib      \
-  ./configure --prefix=${PREFIX} \
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  WITHOUT_X=--without-x
+fi
+
+CPPFLAGS=-I${PREFIX}/includee     \
+LDFLAGS=-L${PREFIX}/lib           \
+  ./configure --prefix=${PREFIX}  \
               --with-launchd-agent-dir=${PREFIX} \
-              --disable-systemd \
-              --disable-selinux \
-              --disable-xml-docs
+              --disable-systemd   \
+              --disable-selinux   \
+              --disable-xml-docs  \
+              ${WITHOUT_X}
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
