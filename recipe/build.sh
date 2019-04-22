a9c103668c9e0023cbfc862444b8c80eedd4fed9
Filipe Fernandes <ocefpaf@gmail.com>
2019-04-22 19:23:24 -0300

    sync and update

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX} \
-    --with-launchd-agent-dir=${PREFIX} \
-    --disable-systemd \
-    --disable-selinux \
-    --disable-xml-docs
-make -j${CPU_COUNT}
-make check
-make install
+CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
+LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"  \
+  ./configure --prefix=${PREFIX}   \
+              --disable-systemd    \
+              --disable-selinux    \
+              --disable-xml-docs   \
+              --with-launchd-agent-dir=${PREFIX}  \
+              --without-x
 
-if [[ -f ${RECIPE_DIR}/post-link.sh.$(uname) ]]; then
-  cp ${RECIPE_DIR}/post-link.sh.$(uname) "${PREFIX}/bin/.dbus-post-link.sh"
+make -j${CPU_COUNT} ${VERBOSE_AT}
+if [[ $(uname) != Darwin ]]; then
+  make check
 fi
+make install
