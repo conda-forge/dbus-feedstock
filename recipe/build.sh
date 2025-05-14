f9dc541058b3141677ea919e09e6fac48dfb0f5b
Caleb Ho <calebh@meta.com>
2025-05-14 15:51:19 -0700

    remove commented code

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-# Get an updated config.sub and config.guess
-# cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./build-aux
-
-# CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
-# LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"  \
-#   ./configure --prefix=${PREFIX}   \
-#               --disable-systemd    \
-#               --disable-selinux    \
-#               --disable-xml-docs   \
-#               --with-launchd-agent-dir=${PREFIX}  \
-#               --without-x
-
-# make -j${CPU_COUNT} ${VERBOSE_AT}
-# if [[ $(uname) != Darwin ]]; then
-#   make check
-# fi
-# make install
 set -euo pipefail
 
 meson setup build \
