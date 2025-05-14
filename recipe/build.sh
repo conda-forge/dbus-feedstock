3c20807ffe87e7e145723d9614a6e89421cb4930
Caleb Ho <calebh@meta.com>
2025-05-14 15:48:22 -0700

    fix build 2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
-mkdir build-aux
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./build-aux
+# cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./build-aux
 
-CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
-LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"  \
-  ./configure --prefix=${PREFIX}   \
-              --disable-systemd    \
-              --disable-selinux    \
-              --disable-xml-docs   \
-              --with-launchd-agent-dir=${PREFIX}  \
-              --without-x
+# CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
+# LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"  \
+#   ./configure --prefix=${PREFIX}   \
+#               --disable-systemd    \
+#               --disable-selinux    \
+#               --disable-xml-docs   \
+#               --with-launchd-agent-dir=${PREFIX}  \
+#               --without-x
 
-make -j${CPU_COUNT} ${VERBOSE_AT}
+# make -j${CPU_COUNT} ${VERBOSE_AT}
+# if [[ $(uname) != Darwin ]]; then
+#   make check
+# fi
+# make install
+set -euo pipefail
+
+meson setup build \
+  --wrap-mode=nodownload \
+  --libdir=lib \
+  -Dprefix="${PREFIX}" \
+  -Dsystemd=disabled \
+  -Dselinux=disabled \
+  -Dxml_docs=disabled \
+  -Dlaunchd_agent_dir="${PREFIX}"
+
+meson compile -C build
 if [[ $(uname) != Darwin ]]; then
-  make check
+  meson test -C build
 fi
-make install
+meson install -C build
